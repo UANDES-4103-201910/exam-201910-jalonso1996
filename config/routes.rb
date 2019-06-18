3b2b7b9129e60801559fd40b413fe231a638b889
Joaquin Alonso <jalonso1@miuandes.cl>
2019-06-18 10:30:19 -0400

    home controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  get 'home/index'
   resources :orders
   resources :addresses
   devise_for :users
   resources :products
+  root to: 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
