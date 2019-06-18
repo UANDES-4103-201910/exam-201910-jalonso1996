c6ffd9bcf891cb81977117d4a207a91c8a19c167
Joaquin Alonso <jalonso1@miuandes.cl>
2019-06-18 12:12:41 -0400

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+user= User.create(firstname: "Joaquin", lastname: "Alonso", email: "jalonso1@miuandes.cl", password: "123123")
+product= Product.create(brand: "Nokia", model: "6.1", variant: "nigga", price: 100000, short_description: "Nokia po", long_description: "El nokia po")
+address= Address.create(phone: 56974316826, line_one: "La capitania 282", city: "Santiago", country: "Chile", zip_code: 1, user: user)
+order= Order.create(user: user)
